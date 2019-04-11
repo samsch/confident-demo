deeacd066a7cff9e84c31d2e7e26fbe962fe3b09
Samuel Scheiderich <samuel.scheiderich@gmail.com>
2019-04-11 16:58:40 -0400

    Not quite working win calculation

--- src/connectFour.js
+++ src/connectFour.js
@@ -18,19 +18,30 @@ export const initialState = {
   winner: null,
 };
 
-const unique = () => `${Date.now}${Math.random()}`;
+const unique = () => `${Date.now()}${Math.random()}`;
+
+const addMessage = (state, text, type = 'error') => {
+  return {
+    ...state,
+    messages: [
+      { id: unique(), text: 'This column is already filled!', type },
+      ...state.messages,
+    ]
+  };
+};
 
 const ColumnFullError = createError('ColumnFullError');
+const GameFinishedError = createError('GameFinishedError');
 
 const handleReducerError = (error, state/*, action */) => {
   if (error instanceof ColumnFullError) {
-    return {
-      ...state,
-      messages: [
-        { id: unique(), text: 'This column is already filled!' },
-        ...state.messages,
-      ]
-    };
+    return addMessage(state, 'This column is already filled!');
+  }
+  if (error instanceof GameFinishedError) {
+    return addMessage(state, state.winner ?
+      `${state.winner === 'black' ? 'Black' : 'Red'} already won! Hit Reset to start a new game.`:
+      'The game was a tie, hit Reset to start a new game!'
+    );
   }
   // eslint-disable-next-line no-console
   console.log('UNKNOWN STATE UPDATE ERROR!', error);
@@ -39,22 +50,61 @@ const handleReducerError = (error, state/*, action */) => {
 const dropPieceInBoard = (board, column, color) => {
   const rowDropPoint = board[column].findIndex(value => value === null);
   if (rowDropPoint < 0) {
-    throw new ColumnFullError('This column is full!');
+    throw new ColumnFullError();
   }
   return set(lensPath([column, rowDropPoint]), color, board);
 };
 
-const findWinner = board => {
-  return null;
+export const allMatch = (board, color, vectors) => {
+  return vectors.every(([x, y]) => {
+    return board[x] && board[x][y] === color;
+  });
+};
+
+const relative = [
+  [[0, 0], [1, 0], [2, 0], [3, 0]], // horizontal
+  [[0, 0], [0, 1], [0, 2], [0, 3]], // vertical
+  [[0, 0], [1, 1], [2, 2], [3, 3]], // diagonal up
+  [[0, 0], [1, -1], [2, -2], [-3, -3]], // diagonal down
+];
+const makeAbsoluteVictories = (x, y) => {
+  return relative.map(vectors => {
+    return vectors.map(([dx, dy]) => {
+      return [x + dx, y + dy];
+    });
+  });
+};
+
+export const findWinner = board => {
+  let winner = null;
+  board.forEach((column, x) => {
+    if (winner) {
+      return;
+    }
+    winner = column.find((color, y) => {
+      if (!color) {
+        return false;
+      }
+      const victoryVectors = makeAbsoluteVictories(x, y);
+      return victoryVectors.some(vectors => {
+        return allMatch(board, board[x, y], vectors);
+      });
+    }) || null;
+  });
+  return winner;
 };
 
 export const reducer = reducerErrors(handleReducerError)((state, action) => {
   if (action.type === 'drop') {
+    if (state.winner) {
+      throw new GameFinishedError();
+    }
     const board = dropPieceInBoard(state.board, action.column, state.nextTurn);
     const winner = findWinner(board);
     return {
       ...state,
       board,
+      messages: winner ? addMessage(state, `${state.winner.toUpperCase()} WINS!`, 'win').messages : state.messages,
       nextTurn: state.nextTurn === 'black' ? 'red' : 'black',
       winner,
     };
---
+++ src/connectFour.test.js
@@ -0,0 +1,80 @@
+/* eslint-env jest */
+import { allMatch, findWinner } from './connectFour';
+
+// Copy from here to make tests
+
+// Bottom left                  top left
+const baseBoard = [
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+];
+// Bottom right                 top right
+
+describe('allMatch logic', () => {
+  test('Should match set of vectors against board', () => {
+    // four characters for text alignment
+    const blak = 'black';
+    const redd = 'red';
+    // Board isn't possible in actual game, but winning positions are.
+    const winningSets = [
+      [blak, null, redd, redd, redd, redd],
+      [null, blak, null, null, null, null],
+      [null, null, blak, null, null, null],
+      [null, redd, redd, blak, null, blak],
+      [null, redd, redd, null, blak, null],
+      [null, redd, redd, blak, null, null],
+      [null, redd, blak, null, null, null],
+    ];
+    const testSets = [
+      [true, blak, [
+        [0, 0],
+      ]],
+      [false, blak, [
+        [0, 1],
+      ]],
+      [true, blak, [
+        [0, 0], [1, 1], [2, 2], [3, 3],
+      ]],
+      [false, redd, [
+        [-1, -1],
+      ]],
+      [true, redd, [
+        [0, 2], [0, 3], [0, 4], [0, 5],
+      ]],
+      [false, redd, [
+        [0, 3], [0, 4], [0, 5], [0, 6],
+      ]],
+      [false, redd, [
+        [3, 2], [4, 2], [5, 2], [6, 2],
+      ]],
+    ];
+    testSets.forEach(([shouldBe, color, vectors]) => {
+      const result = allMatch(winningSets, color, vectors);
+      expect(result).toBe(shouldBe);
+    });
+  });
+});
+
+describe('findWinner logic', () => {
+  test('Should find winner', () => {
+    // four characters for text alignment
+    const blak = 'black';
+    const redd = 'red';
+    // Board isn't possible in actual game, but winning positions are.
+    const winningSet = [
+      [blak, null, redd, redd, redd, redd],
+      [null, blak, null, null, null, null],
+      [null, null, blak, null, null, null],
+      [null, redd, redd, blak, null, blak],
+      [null, redd, redd, null, blak, null],
+      [null, redd, redd, blak, null, null],
+      [null, redd, blak, null, null, null],
+    ];
+    expect(findWinner(winningSet)).toBe('red');
+  });
+});
