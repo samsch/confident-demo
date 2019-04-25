cabcee84400c2fcd4fcc531978b17a8c248a8437
Samuel Scheiderich <samuel.scheiderich@gmail.com>
2019-04-25 15:30:13 -0400

    Setup gh-pages branch

--- src/util/reducerErrors.js
+++
@@ -1,7 +0,0 @@
-export default handleError => reducer => (state, action) => {
-  try {
-    return reducer(state, action);
-  } catch (error) {
-    return handleError(error, state, action);
-  }
-};
